b56f8ec7f30d8f25a1018db4cf40a4401d4e6b88
brainstream <brainstream@yandex.ru>
2023-04-02 13:20:00 +0400

    Questions from DB

--- mybreng-api/database/__init__.py
+++ mybreng-api/database/__init__.py
@@ -8,10 +8,10 @@ class UserTable(db.Model):
     id = db.Column('Id', db.String(38), primary_key=True)
     email = db.Column('Email', db.String(250))
     password_hash = db.Column('PasswordHash', db.String(128))
-    password_solt = db.Column('PasswordSolt', db.String(32))
+    password_salt = db.Column('PasswordSalt', db.String(32))
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User: {self.email}>'
 
 
 class QuizTable(db.Model):
@@ -21,6 +21,19 @@ class QuizTable(db.Model):
     description = db.Column('Description', db.String)
     owner_id = db.Column('Owner', db.ForeignKey('User.Id'))
     owner = db.relationship('UserTable', backref=db.backref('User', uselist=False))
+    questions = db.relationship("QuizQuestionTable", backref=db.backref('QuizQuestion', uselist=False))
 
     def __repr__(self):
-        return f'<Quiz {self.title}>'
+        return f'<Quiz: {self.title}>'
+
+
+class QuizQuestionTable(db.Model):
+    __tablename__ = 'QuizQuestion'
+    id = db.Column('Id', db.String(38), primary_key=True)
+    text = db.Column('Text', db.String)
+    type = db.Column('Type', db.Integer)
+    ordinal_number = db.Column('OrdinalNumber', db.Integer)
+    quiz_id = db.Column('Quiz', db.ForeignKey('Quiz.Id'))
+
+    def __repr__(self):
+        return f'<Quiz Question: {self.description[:20]}>'
--- mybreng-api/facades/quiz_facade.py
+++ mybreng-api/facades/quiz_facade.py
@@ -1,5 +1,5 @@
-from database import QuizTable
-from dtos import QuizDto, QuizDetailedDto
+from database import QuizTable, QuizQuestionTable
+from dtos import QuizDto, QuizDetailedDto, QuizQuestionDto, QuizQuestionType
 
 
 # noinspection PyMethodMayBeStatic
@@ -17,4 +17,26 @@ class QuizFacade:
         return self._map_to_quiz_details(quiz)
 
     def _map_to_quiz_details(self, quiz: QuizTable) -> QuizDetailedDto:
-        return QuizDetailedDto(quiz.id, quiz.title, quiz.description, [])
+        return QuizDetailedDto(
+            quiz.id,
+            quiz.title,
+            quiz.description,
+            [self._map_to_quiz_question(question) for question in quiz.questions]
+        )
+
+    def _map_to_quiz_question(self, question: QuizQuestionTable) -> QuizQuestionDto:
+        return QuizQuestionDto(
+            question.id,
+            question.text,
+            self._map_to_question_type(question.type),
+            question.ordinal_number
+        )
+
+    def _map_to_question_type(self, db_type: int) -> QuizQuestionType:
+        match db_type:
+            case 1:
+                return QuizQuestionType.MULTIPLE_CHOICE
+            case 2:
+                return QuizQuestionType.FREE_TEXT
+            case _:
+                return QuizQuestionType.SINGLE_CHOICE
