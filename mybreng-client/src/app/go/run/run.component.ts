65c2304e759fa2f8f09b51a721690d18d974be30
brainstream <brainstream@yandex.ru>
2023-05-20 19:11:34 +0400

    The Run component initialization

--- mybreng-client/src/app/go/run/run.component.ts
+++ mybreng-client/src/app/go/run/run.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { goActions } from '../store';
 
 @Component({
     selector: 'app-run',
     templateUrl: './run.component.html',
     styleUrls: ['./run.component.scss']
 })
-export class RunComponent {
+export class RunComponent implements OnInit, OnDestroy {
+    private readonly subscriptions: Subscription[] = [];
 
+    constructor(
+        private readonly route: ActivatedRoute,
+        private readonly store$: Store
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.subscriptions.push(this.route.paramMap.subscribe(params => {
+            const id = params.get('id');
+            if (id) {
+                this.store$.dispatch(goActions.load({ id }))
+            }
+        }));
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.forEach(s => s.unsubscribe());
+        this.subscriptions.splice(0, this.subscriptions.length);
+    }
 }
--- mybreng-client/src/app/go/store/go-effects.ts
+++ mybreng-client/src/app/go/store/go-effects.ts
@@ -1,6 +1,50 @@
 import { Injectable } from "@angular/core";
+import { MessageService } from "@app/common";
+import { RunService } from "@app/web-api";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { Store } from "@ngrx/store";
+import { GoEventsService } from "./go-events.service";
+import { catchError, concat, map, of, switchMap, tap } from "rxjs";
+import { watchHttpErrors } from "@app/shared";
+import { goActions } from "./go-actions";
 
 @Injectable()
 export class GoEffects {
+    constructor(
+        private readonly actions$: Actions,
+        private store$: Store,
+        private readonly runService: RunService,
+        private readonly messageService: MessageService,
+        private readonly eventsService: GoEventsService
+    ) {
+    }
 
+    showError$ = createEffect(() => this.actions$.pipe(
+        ofType(goActions.setError),
+        tap(({ message }) => {
+            this.messageService.showError(message)
+        })
+    ), { dispatch: false });
+
+    flushEvents$ = createEffect(() => this.actions$.pipe(
+        ofType(goActions.flushEvents),
+        tap(({ events }) => {
+            events.forEach(e => e.flush());
+        })
+    ), { dispatch: false });
+
+    loadDetails$ = createEffect(() => this.actions$.pipe(
+        ofType(goActions.load),
+        switchMap(({ id }) => concat(
+            of(goActions.setLoading({ loading: true })),
+            watchHttpErrors(this.runService.runGet(id, 'events'))
+                .pipe(
+                    map(run => goActions.loaded({ run })),
+                    catchError(() => of(goActions.setError({
+                        message: 'Во время загрузки данных произошла ошибка'
+                    })))
+                ),
+            of(goActions.setLoading({ loading: false }))
+        ))
+    ));
 }
