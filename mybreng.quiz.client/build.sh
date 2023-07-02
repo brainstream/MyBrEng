5a66024769c6fb8d70710af3b95c4846164634ad
brainstream <brainstream@yandex.ru>
2023-07-02 15:27:50 +0400

    Publication fixes

---
+++ mybreng.quiz.client/build.sh
@@ -0,0 +1,2 @@
+./generate-web-api.sh -u https://api.brainstream.site
+npm run build
--- static-server/main.py
+++ static-server/main.py
@@ -1,12 +1,17 @@
+import pathlib
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def root(path):
-    return send_from_directory('static', 'index.html')
+    ext = pathlib.Path(path).suffix
+    if ext == '.css' or ext == '.js' or ext == '.ico':
+        return send_from_directory('static', path)
+    else:
+        return send_from_directory('static', 'index.html')
 
 
 if __name__ == '__main__':
