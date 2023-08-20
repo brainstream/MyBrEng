93bf5992ad2755a64b01e42b40b1de7bbd50d1e1
brainstream <brainstream@yandex.ru>
2023-08-20 21:53:45 +0400

    Test data

---
+++ database/test-data/test-data.sql
@@ -0,0 +1,11 @@
+INSERT INTO `user` (`id`, `email`, `password_hash`, `password_salt`) 
+VALUES (
+    '5b96d797-3ed1-11ee-8891-c5d1c875f25d', 
+    'user@test.test',
+    'w+V8Qlj2Cqoi6+UHC46wXU2CsQKBUjBJatOQWQhOrQtQniapzHp2gCI/LKTD677v2anUfTDgPelQlb8Oc5zdEA==',
+    'theiZ9eo4goraaqu7ahw7aiFoh0Eefop'
+)
+
+
+
+
