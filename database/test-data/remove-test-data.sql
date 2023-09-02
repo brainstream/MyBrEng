cb834f0be4c6b213e4d982e70107355c6f6bbb8e
brainstream <brainstream@yandex.ru>
2023-09-02 23:28:58 +0400

    Test data added

---
+++ database/test-data/remove-test-data.sql
@@ -0,0 +1,2 @@
+DELETE FROM `quiz` WHERE `owner` = '5b96d797-3ed1-11ee-8891-c5d1c875f25d';
+DELETE FROM `user` WHERE `id` = '5b96d797-3ed1-11ee-8891-c5d1c875f25d';
--- database/test-data/test-data.sql
+++ database/test-data/test-data.sql
@@ -1,3 +1,5 @@
+-- Email:    user@test.test
+-- Password: 123456
 INSERT INTO `user` (`id`, `email`, `password_hash`, `password_salt`) 
 VALUES (
     '5b96d797-3ed1-11ee-8891-c5d1c875f25d', 
@@ -6,6 +8,21 @@ VALUES (
     'theiZ9eo4goraaqu7ahw7aiFoh0Eefop'
 )
 
-
-
+INSERT INTO `quiz` (`id`, `title`, `description`, `owner`)
+VALUES (
+	uuid(),
+	'Fun with the Alphabet!',
+	'How well do you know your ABC in English? Play the game and see!',
+	'5b96d797-3ed1-11ee-8891-c5d1c875f25d'
+), (
+	uuid(),
+	'Animals Picture Quiz',
+	'Can you name these animals?',
+	'5b96d797-3ed1-11ee-8891-c5d1c875f25d'
+), (
+	uuid(),
+	'Synonyms Quiz',
+	'Choose words from the list and match them with words that have similar meanings (synonyms).',
+	'5b96d797-3ed1-11ee-8891-c5d1c875f25d'
+)
 
