3d6e36a7356045ec52c7811e22d167cede46bcfc
brainstream <brainstream@yandex.ru>
2025-08-23 22:31:25 +0300

    Autocomplete fixed

--- mybreng.quiz.client/src/app/student/student-add-run-form/student-add-run-form.component.ts
+++ mybreng.quiz.client/src/app/student/student-add-run-form/student-add-run-form.component.ts
@@ -1,9 +1,10 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { StudentsEventsService, StudentsSelectors, studentsActions } from '../store';
-import { Observable, Subscription } from 'rxjs';
+import { combineLatest, map, Observable, startWith, Subscription } from 'rxjs';
 import { QuizDto } from '@app/web-api';
 import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-student-add-run-form',
@@ -13,20 +14,39 @@ import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bott
 })
 export class StudentAddRunFormComponent implements OnInit, OnDestroy {
     private createdSubscription: Subscription | null = null;
+    private readonly allAvailableQuizzes$: Observable<QuizDto[]>;
 
-    readonly availableQuizzes$: Observable<QuizDto[]>;
+    readonly form: FormGroup;
+    availableQuizzes$: Observable<QuizDto[]>;
     selectedQuiz?: QuizDto;
 
     constructor(
         private readonly store$: Store,
         private readonly events: StudentsEventsService,
         private readonly bottomSheet: MatBottomSheetRef,
+        formBuilder: FormBuilder,
         @Inject(MAT_BOTTOM_SHEET_DATA) private readonly student: { studentId: string }
     ) {
-        this.availableQuizzes$ = store$.select(StudentsSelectors.availableQuizzes);
+        this.allAvailableQuizzes$ = store$.select(StudentsSelectors.availableQuizzes);
+        this.form = formBuilder.group({
+            quiz: ['', Validators.required]
+        });
     }
 
     ngOnInit(): void {
+        this.availableQuizzes$ = combineLatest([
+            this.form.controls['quiz'].valueChanges.pipe(startWith('')),
+            this.allAvailableQuizzes$
+        ])
+        .pipe(
+            map(([searchString, quizzes]) => {
+                if (typeof searchString != 'string') {
+                    return quizzes;
+                }
+                const lowerCaseSearchString = searchString.toLocaleLowerCase();
+                return quizzes.filter(q => q.title.toLocaleLowerCase().includes(lowerCaseSearchString));
+            })
+        );
         this.store$.dispatch(studentsActions.loadAvailableQuizzes());
         this.createdSubscription = this.events.runCreated$.subscribe(() => {
             this.bottomSheet.dismiss();
@@ -41,22 +61,20 @@ export class StudentAddRunFormComponent implements OnInit, OnDestroy {
         return quiz?.title ?? '';
     }
 
-    get valid(): boolean {
-        return this.selectedQuiz != undefined;
-    }
-
-    add(): void {
-        if (this.selectedQuiz) {
+    add(): boolean {
+        if (this.form.valid) {
             this.store$.dispatch(studentsActions.addRun({
                 run: {
-                    quizId: this.selectedQuiz.id,
+                    quizId: this.form.controls['quiz'].value.id,
                     studentId: this.student.studentId
                 }
             }));
         }
+        return false;
     }
 
-    cancel(): void {
+    cancel(): boolean {
         this.bottomSheet.dismiss();
+        return false;
     }
 }
